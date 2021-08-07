8a99d544fb63d1375dfa325e21524ec8cd5f80be
leonardo.sales@burgerking.com.br <leonardo.sales@burgerking.com.br>
2021-08-07 21:42:34 +0000

    criação da dimensao divisao na tabela stg_suppliers.sql

--- models/staging/stg_suppliers.sql
+++ models/staging/stg_suppliers.sql
@@ -1,5 +1,6 @@
 with fornecedores as (
     select
+    row_number() over (order by suppliers.supplier_id) as  sk_supplier,
     country as pais,
     city as cidade,
     postal_code as cep,
@@ -9,9 +10,29 @@ with fornecedores as (
     contact_name as contato_fornecedor,
     phone as telefone,
     company_name as nome_fornecedor,
-    contact_title as cargo_fornecedor
+    contact_title as cargo_fornecedor, 
+    case
+        when country = "Australia" then "Asia"
+        when country = "Brazil" then "LATAM"
+        when country = "Denmark" then "Europe"
+        when country = "Finland" then "Europe"
+        when country = "France" then "Europe"
+        when country = "Germany" then "Europe"
+        when country = "Italy" then "Europe"
+        when country = "Japan" then "Asia"
+        when country = "Netherlands" then "Europe"
+        when country = "Norway" then "Europe"
+        when country = "Singapore" then "Asia"
+        when country = "Spain" then "Europe"
+        when country = "Sweden" then "Europe"
+        when country = "UK" then "Europe"
+        when country = "USA" then "North America"       
+    end as divisao
+
 
     from {{ source('northwind_rawdata', 'suppliers')}}
 )
 
 select * from fornecedores
+
+
